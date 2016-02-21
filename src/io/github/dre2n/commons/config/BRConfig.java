5846fb869df9fee81b55364c90164d55f11b3c14
Daniel Saukel <sa.da@unitybox.de>
2016-02-21 13:06:40 +0100

    BRConfig improvements

--- src/io/github/dre2n/commons/config/BRConfig.java
+++ src/io/github/dre2n/commons/config/BRConfig.java
@@ -1,15 +1,20 @@
 package io.github.dre2n.commons.config;
 
+import io.github.dre2n.commons.util.messageutil.MessageUtil;
+
 import java.io.File;
 import java.io.IOException;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
-public class BRConfig {
+public abstract class BRConfig {
+	
+	public int CONFIG_VERSION;
 	
 	protected File file;
 	protected FileConfiguration config;
+	protected int configVersion;
 	
 	public BRConfig(File file) {
 		this.file = file;
@@ -18,13 +23,24 @@ public class BRConfig {
 			try {
 				file.getParentFile().mkdirs();
 				file.createNewFile();
+				config = YamlConfiguration.loadConfiguration(file);
+				config.set("configVersion", CONFIG_VERSION);
+				initialize();
 				
 			} catch (IOException exception) {
 				exception.printStackTrace();
 			}
+			
+		} else {
+			config = YamlConfiguration.loadConfiguration(file);
+			load();
+			if (configVersion != CONFIG_VERSION) {
+				MessageUtil.log("The configuration file " + file.getName() + " seems to be outdated.");
+				MessageUtil.log("Adding missing values...");
+				initialize();
+				config.set("configVersion", CONFIG_VERSION);
+			}
 		}
-		
-		config = YamlConfiguration.loadConfiguration(file);
 	}
 	
 	/**
@@ -40,5 +56,24 @@ public class BRConfig {
 	public FileConfiguration getConfig() {
 		return config;
 	}
+	
+	/**
+	 * @return the configuration version
+	 */
+	public int getConfigVersion() {
+		return configVersion;
+	}
+	
+	// Abstracts
+	
+	/**
+	 * Initial setup
+	 */
+	public abstract void initialize();
+	
+	/**
+	 * Load the config from file
+	 */
+	public abstract void load();
 	
 }
