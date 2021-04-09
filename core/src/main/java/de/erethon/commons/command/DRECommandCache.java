bb26e30873c00587837cd625f4014efe677b3cb3
Fyreum <luka.vogel@gmx.de>
2021-04-10 00:00:49 +0200

    Removed unnecessary import

--- core/src/main/java/de/erethon/commons/command/DRECommandCache.java
+++ core/src/main/java/de/erethon/commons/command/DRECommandCache.java
@@ -20,7 +20,6 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
